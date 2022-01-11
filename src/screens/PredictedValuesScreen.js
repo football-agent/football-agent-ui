87a999ec2eb33a3ebd3888c41572118f9c40ed26
Sanskar Gupta <snskr95@gmail.com>
2022-01-11 09:22:35 +0100

    added some hardcoded text

--- src/screens/PredictedValuesScreen.js
+++ src/screens/PredictedValuesScreen.js
@@ -10,6 +10,8 @@ export default function PredictedValuesScreen() {
     <div>
       <p>Selected Player: {state.selectedPlayer.player}</p>
       <p>Selected Team: {state.selectedTeam.team}</p>
+      <p>The predicted value is 1000000</p>
+      <p>The player performs 10 percent better than rest of the team </p>
     </div>
   );
 }
