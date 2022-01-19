f64a8da9d8867dabcb4d6c1ab9e4f1d546a38c9e
Sanskar Gupta <snskr95@gmail.com>
2022-01-19 20:08:46 +0100

    added grid on prediction values screen

--- src/screens/PredictedValuesScreen.js
+++ src/screens/PredictedValuesScreen.js
@@ -1,17 +1,68 @@
-import React, { useContext } from "react";
-import { useSelectionContext } from '../context/SelectionProvider'
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default function PredictedValuesScreen() {
-    const {
-        state
-      } = useSelectionContext()
   return (
-    <div>
-      {/* <p>Selected Player: {state.selectedPlayer.player}</p>
-      <p>Selected Team: {state.selectedTeam.team}</p>
-      <p>The predicted value is 1000000</p>
-      <p>The player performs 10 percent better than rest of the team </p> */}
-    </div>
+    <Box style={{ margin: "2rem" }}>
+      <Grid container>
+        <Grid item xs={8}>
+          <Card style={{ backgroundColor: "#faf7f2" }}>
+            <CardContent>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <Avatar
+                  sx={{ bgcolor: "red", width: 100, height: 130 }}
+                  variant="square"
+                >
+                  N
+                </Avatar>
+                <div style={{ margin: "1rem" }}>
+                  <p>Player Name</p>
+                  <p>Age</p>
+                  <p>Club</p>
+                  <p>Nationality</p>
+                </div>
+
+                <div
+                  style={{
+                    backgroundColor: "grey",
+                    border: "2px solid red",
+                    height: "50px",
+                    marginLeft: "4rem",
+                  }}
+                >
+                  Predicted Value
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Item>Team based statistic</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>League based statistic</Item>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Item>The magic behind our prediction</Item>
+        </Grid>
+
+
+      </Grid>
+    </Box>
   );
 }
